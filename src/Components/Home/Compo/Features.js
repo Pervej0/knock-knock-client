f225b65216ba74b70afded372dc62fd72b20c23f
Pervej0 <mdparvez222khan@gmail.com>
2021-10-30 02:57:39 +0600

    Feature section design modified

--- src/Components/Home/Compo/Features.js
+++ src/Components/Home/Compo/Features.js
@@ -1,4 +1,10 @@
-import { faClock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faClock,
+  faMapMarked,
+  faMobile,
+  faUser,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
@@ -13,37 +19,56 @@ const Features = () => {
             alt=""
           />
         </div>
-        <div className="bg-gray-800 text-left flex justify-center items-center flex-col pl-12 font-bold">
-          <h1 className="text-3xl md:text-5xl text-white mb-6">
-            Sit at Home
-            <br />
-            <span className="text-yellow-400">We Will Take Care</span>
-          </h1>
-          <p>
-            Express delivery is the fastest form of shipping. The customer pays
-            an extra shipping cost for this type of delivery, as the shipment
-            will get transported to him anywhere between 24 to 72 hours
-          </p>
-          <div className="flex flex-wrap gap-8 my-12">
-            <div className="text-center">
-              <FontAwesomeIcon icon={faClock} size="2x" />
-              <h5>Fast Delivery</h5>
-              <p>in 1 hour</p>
-            </div>
-            <div className="text-center">
-              <FontAwesomeIcon icon={faClock} size="2x" />
-              <h5>Fast Delivery</h5>
-              <p>in 1 hour</p>
-            </div>
-            <div className="text-center">
-              <FontAwesomeIcon icon={faClock} size="2x" />
-              <h5>Fast Delivery</h5>
-              <p>in 1 hour</p>
-            </div>
-            <div className="text-center">
-              <FontAwesomeIcon icon={faClock} size="2x" />
-              <h5>Fast Delivery</h5>
-              <p>in 1 hour</p>
+        <div className="bg-gray-800 text-left flex items-center font-bold">
+          <div className="md:px-10 p-3">
+            <h1 className="text-3xl md:text-5xl text-white mb-6">
+              Sit at Home
+              <br />
+              <span className="text-yellow-400">We Will Take Care</span>
+            </h1>
+            <p className="text-lg text-white">
+              Express delivery is the fastest form of shipping. The customer
+              pays an extra shipping cost for this type of delivery, as the
+              shipment will get transported to him anywhere between 24 to 72
+              hours
+            </p>
+            <div className="flex flex-wrap gap-8 my-12">
+              <div className="text-center">
+                <button className="text-yellow-500 hover:mt-4">
+                  <FontAwesomeIcon icon={faMapMarked} size="3x" />
+                </button>
+                <h5 className="text-white mt-4">
+                  Wide coverage <br />
+                  area
+                </h5>
+              </div>
+              <div className="text-center">
+                <button className="text-yellow-500 hover:mt-4">
+                  <FontAwesomeIcon icon={faClock} size="3x" />
+                </button>
+                <h5 className="text-white mt-4">
+                  Fast Delivery <br />
+                  in 1 hour
+                </h5>
+              </div>
+              <div className="text-center">
+                <button className="text-yellow-500 hover:mt-4">
+                  <FontAwesomeIcon icon={faMobile} size="3x" />
+                </button>
+                <h5 className="text-white mt-4">
+                  Amazing <br />
+                  Mobile App
+                </h5>
+              </div>
+              <div className="text-center">
+                <button className="text-yellow-500 hover:mt-4">
+                  <FontAwesomeIcon icon={faUsers} size="3x" />
+                </button>
+                <h5 className="text-white mt-4">
+                  150 + member <br />
+                  family
+                </h5>
+              </div>
             </div>
           </div>
         </div>
